440365f345dd2b6959b9aea5dabf0a12fb0b033b
HelloYu <Mr.Yu1991@gmail.com>
2016-12-02 00:28:19 +0800

    - added loading directive

---
+++ app/shared/directives/loading.js
@@ -0,0 +1,71 @@
+(function(angular) {
+    'use strict';
+
+    angular
+        .module('app.shared')
+        .directive('loading', loading);
+
+    /**
+     * @class loading
+     * ## 使用说明
+     *      <div loading="vm.loading" ><div>被包裹的内容</div></div>
+     * 
+     * 注：外层元素必须要有高度和宽度，这样 Loading 才能自适应
+     */
+    loading.$inject = [];
+
+    /* @ngInject */
+    function loading() {
+
+        var loading = {
+
+            link: link,
+            restrict: 'A',
+            scope: {
+                /**
+                 * 设置Loading状态，true显示，false隐藏
+                 * @property {Boolean}   
+                 */
+                loading: '=',
+            }
+        };
+        return loading;
+
+        function link(scope, element, attrs) {
+
+            var options = {
+                /**
+                 * @cfg 选择显示的模板
+                 */
+                template: 1,
+
+            };
+
+            var loading = $('<div></div>');
+
+            loading.append(loadingTemplates[options.template]);
+
+            loading.addClass('loading');
+
+            element.css('position', 'relative');
+            element.append(loading);
+
+            scope.$watch('loading', function(newVal, oldVal) {
+                if (newVal != undefined) {
+                    if (newVal) {
+                        loading.show();
+                    } else {
+                        loading.hide();
+                    }
+                }
+            });
+
+        }
+    }
+
+    var loadingTemplates = {
+        1: '<div class="loading-container"><div class="loader-container"><span class="loader loader-quart"></span><p>加载中...</p></div></div>',
+
+    }
+
+})(window.angular);
