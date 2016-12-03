2282d30d0f14e5f9bdadf878e2594dbebeee0755
HelloYu <Mr.Yu1991@gmail.com>
2016-12-03 18:54:37 +0800

    - added goods-class template

--- app/goods-management/goods-class/goods-class.controller.js
+++ app/goods-management/goods-class/goods-class.controller.js
@@ -5,22 +5,48 @@
         .module('app.goods_management')
         .controller('GoodsClassCtrl', GoodsClassCtrl);
 
-    GoodsClassCtrl.$inject = ['UtilityService'];
+    GoodsClassCtrl.$inject = ['UtilityService', 'GoodsModel'];
 
-    function GoodsClassCtrl(UtilityService) {
+    function GoodsClassCtrl(UtilityService, GoodsModel) {
         var vm = this;
         /*----------  界面层资源  ----------*/
-
+        // 当前选中商品分类
+        vm.selectedGoodsClass;
+        // 当前新增商品分类
+        vm.currentGoodsClass;
+        // 商品分类列表
+        vm.list;
         /*----------  内部变量  ----------*/
 
-        var utilityService = UtilityService;
+        var utilityService = UtilityService,
+            goodsModel = GoodsModel;
         /*----------  内部逻辑函数  ----------*/
 
 
         /*----------  内部辅助函数  ----------*/
 
-        function init() {
+        function initGoodsClass() {
+            var goodsClass = {
+                "store": null,
+                "operator": null,
+                "name": "",
+                "parent": null,
+                "remark": ""
+            }
+            return goodsClass;
+        }
 
+        function initGoodsClassList() {
+            goodsModel.getClasses().then(result => {
+                vm.list = result;
+            });
+        }
+
+        function init() {
+            // 初始化分类对象
+            vm.currentGoodsClass = initGoodsClass();
+            // 初始化分类列表
+            initGoodsClassList();
         }
 
         init();
