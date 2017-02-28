5019fa7bc06c02e173ee0212867cc648ce75316e
HelloYu <Mr.Yu1991@gmail.com>
2017-02-28 13:02:01 +0800

    - updated member-level module

--- app/member-management/member-level/member-level.controller.js
+++ app/member-management/member-level/member-level.controller.js
@@ -5,10 +5,10 @@
         .module('app.member_management')
         .controller('MemberLevelCtrl', MemberLevelCtrl);
 
-    MemberLevelCtrl.$inject = ['UtilityService', 'MemberModel', '$uibModal'];
+    MemberLevelCtrl.$inject = ['UtilityService', 'MemberModel', '$uibModal', 'Restangular'];
 
-    function MemberLevelCtrl(UtilityService, MemberModel, $uibModal) {
-        var vm = this;
+    function MemberLevelCtrl(UtilityService, MemberModel, $uibModal, Restangular) {
+        let vm = this;
         /*----------  界面层资源  ----------*/
 
         //会员等级列表
@@ -19,11 +19,55 @@
         };
 
         vm.toggleItems = toggleItems;
+        vm.openCreateModal = openCreateModal;
+        vm.openEditModal = openEditModal;
+        vm.openRemoveModal = openRemoveModal;
         /*----------  内部变量  ----------*/
 
-        var utilityService = UtilityService,
-            memberModel = MemberModel;
+        let utilityService = UtilityService,
+            memberModel = MemberModel,
+            restangular = Restangular;
+
+        let status = {
+            edit: {
+                name: '编辑',
+                save: saveEdit
+            },
+            create: {
+                name: '添加',
+                save: saveCreate
+            }
+        };
+
         /*----------  内部逻辑函数  ----------*/
+
+        function saveEdit(member) {
+
+        }
+
+        function saveCreate(member) {
+
+        }
+
+        function remove(items) {
+
+        }
+
+        function openRemoveModal(items) {
+            let that = vm;
+            $uibModal.open({
+                templateUrl: 'app/member-management/member-level/remove.modal.html',
+                controller: function($scope) {
+                    let selected = utilityService.getSelected(items);
+                    let vm = {};
+
+                    vm.list = selected;
+                    vm.remove = remove;
+
+                    $scope.vm = vm;
+                }
+            });
+        }
 
         function toggleItems(items) {
             utilityService.toggleItems(items, vm.current.selectedAll);
@@ -41,28 +85,43 @@
                     templateUrl: 'app/shared/views/system-notice.tpl.html',
                     size: 'sm',
                     controller: function($scope) {
+
                         $scope.title = '系统提示';
-                        $scope.content = '同时只能选中一个会员进行编辑！';
+                        $scope.content = '同时只能选中一个会员等级进行编辑！';
                     }
                 });
                 return;
             }
+
+            let that = vm;
             $uibModal.open({
-                templateUrl: 'app/member-management/member-list/edit.modal.html',
-                size: 'lg',
+                templateUrl: 'app/member-management/member-level/create-edit.modal.html',
                 controller: function($scope) {
-                    let member = restangular.copy(selected[0]);
-                    $scope.vm = {};
+                    let level = restangular.copy(selected[0]);
+                    let vm = {};
+                    debugger;
+                    vm.level = level;
+                    vm.status = status.edit;
+                    vm.list = that.list;
+                    $scope.vm = vm;
+                }
+            });
+        }
 
-                    $scope.vm.member = member;
-                    $scope.vm.confirmPaymentPassword = member.payment_password;
-                    $scope.vm.hidePaymentPasswordNotice = true;
-                    $scope.vm.edit = edit;
-                    $scope.vm.levelList = vm.levelList;
-                    $scope.vm.checkPaymentPassword = (password, confirmPassword) => {
+        /**
+         * 打开会员等级新增窗口
+         */
+        function openCreateModal() {
+            let that = vm;
+            $uibModal.open({
+                templateUrl: 'app/member-management/member-level/create-edit.modal.html',
+                controller: function($scope) {
+                    let vm = {};
+                    vm.level = initMemberLevel();
 
-                        $scope.vm.hidePaymentPasswordNotice = memberService.checkPassword(password, confirmPassword);
-                    }
+                    vm.list = that.list;
+                    vm.status = status.create;
+                    $scope.vm = vm;
                 }
             });
         }
@@ -70,7 +129,7 @@
         /*----------  内部辅助函数  ----------*/
 
         function initMemberLevel() {
-            var memberLevel = {
+            let memberLevel = {
                 "store": null,
                 "operator": null,
                 "name": "",
@@ -94,7 +153,7 @@
         }
 
         function init() {
-            vm.currentMemberLevel = initMemberLevel();
+
             initMemberLevelList();
         }
 
