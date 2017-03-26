948120ec32c4e72f4b3d8d6ffe718c1f57577528
HelloYu <Mr.Yu1991@gmail.com>
2017-03-26 10:11:53 +0800

    - updated member register module

--- build/app.js
+++ build/app.js
@@ -1673,43 +1673,6 @@ $(function() {
 
     angular
         .module('app.goods_management')
-        .controller('InventoryListCtrl', InventoryListCtrl);
-
-    InventoryListCtrl.$inject = ['UtilityService', 'GoodsModel'];
-
-    function InventoryListCtrl(UtilityService, GoodsModel) {
-        var vm = this;
-        /*----------  界面层资源  ----------*/
-        vm.pagination;
-        vm.list;
-        /*----------  内部变量  ----------*/
-
-        var utilityService = UtilityService,
-            goodsModel = GoodsModel;
-        /*----------  内部逻辑函数  ----------*/
-
-
-        function getInventories(configs) {
-            goodsModel.getInventories(configs).then((result) => {
-                result = result.plain();
-                vm.list = result;
-            });
-        }
-        /*----------  内部辅助函数  ----------*/
-
-        function init() {
-            vm.pagination = utilityService.initPagination();
-            getInventories(vm.pagination.configs);
-        }
-
-        init();
-    }
-})();
-(function() {
-    'use strict';
-
-    angular
-        .module('app.goods_management')
         .controller('GoodsRegisterCtrl', GoodsRegisterCtrl);
 
     GoodsRegisterCtrl.$inject = ['GoodsModel', 'DictionaryService', '$filter', 'UtilityService'];
@@ -1809,6 +1772,43 @@ $(function() {
             utilityService.getDatetime().then(result => {
                 vm.currentGoods.code = result;
             });
+        }
+
+        init();
+    }
+})();
+(function() {
+    'use strict';
+
+    angular
+        .module('app.goods_management')
+        .controller('InventoryListCtrl', InventoryListCtrl);
+
+    InventoryListCtrl.$inject = ['UtilityService', 'GoodsModel'];
+
+    function InventoryListCtrl(UtilityService, GoodsModel) {
+        var vm = this;
+        /*----------  界面层资源  ----------*/
+        vm.pagination;
+        vm.list;
+        /*----------  内部变量  ----------*/
+
+        var utilityService = UtilityService,
+            goodsModel = GoodsModel;
+        /*----------  内部逻辑函数  ----------*/
+
+
+        function getInventories(configs) {
+            goodsModel.getInventories(configs).then((result) => {
+                result = result.plain();
+                vm.list = result;
+            });
+        }
+        /*----------  内部辅助函数  ----------*/
+
+        function init() {
+            vm.pagination = utilityService.initPagination();
+            getInventories(vm.pagination.configs);
         }
 
         init();
